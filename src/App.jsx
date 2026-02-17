d14383d76930dd6c8541fbca5a6e618f47ab43a3
brwnkn <brownkaineforchick0@gmail.com>
2026-02-16 20:23:02 -0500

    Successfully implemented the drawing logic on the canvas.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,75 @@
 
 import './App.css'
+import { useRef, useEffect, use} from 'react'
+
+
 
 function App() {
+  const canvasRef = useRef(null);
+  const solveRef = useRef(null);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+    const drawingBoard = canvasRef.current
+    const ctx = drawingBoard.getContext('2d');
+
+    let isPressed = false;
+    const brushSize = 5;
+    let x = 0;
+    let y = 0;
+    const brushColor = 'black';
+    //...
+    let rect = drawingBoard.getBoundingClientRect();
+    drawingBoard.width = rect.width; 
+    drawingBoard.height = rect.height;
+    // the enclosed segment above enables getting exact CSS scale for the canvas so the drawing logic works properly.
+    const drawCircle=(x, y)=>{
+        ctx.beginPath();
+        ctx.arc(x, y, brushSize, 0, Math.PI * 2);
+        ctx.fillStyle = brushColor;
+        ctx.fill();
+    };
+
+    const drawLine=(x1, y1, x2, y2) =>  {
+        ctx.beginPath();
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+        ctx.strokeStyle = brushColor;
+        ctx.lineWidth = brushSize * 2;
+        ctx.stroke();
+    };
+
+    drawingBoard.addEventListener('mousedown', (event) => {
+        isPressed = true;
+        x = event.offsetX;
+        y = event.offsetY;
+    });
+
+    drawingBoard.addEventListener('mouseup', (event) => {
+        isPressed = false;
+        x = undefined;
+        y = undefined;
+    });
+
+    drawingBoard.addEventListener('mousemove', (event) => {
+        if(isPressed){
+            const x2 = event.offsetX;
+            const y2 = event.offsetY;
+            drawCircle(x2, y2);
+            drawLine(x, y, x2, y2);
+            x = x2;
+            y = y2;
+        }
+    });
+
+    solveRef.current.addEventListener('click', () => {
+      console.log("solve button clicked");
+    });
+
+
+      }, []);
+
+//the UI display part, not related to the drawing logic
   return (
     <div className="App">
       <div className = "topPanel">
@@ -20,13 +88,13 @@ function App() {
           <button className = "clearButton">
             <img src ="trash.svg"></img>
           </button>
-          <button className = "solveButton">
+          <button className = "solveButton" ref = {solveRef}>
             <img src ="solve.svg"></img>
           </button>
         </div>
 
         <div className = "canvasPanel">
-          <canvas id = "canvas"></canvas>
+          <canvas id = "canvas"  ref = {canvasRef} ></canvas>
         </div>
 
         <div className = "solutionPanel">
@@ -44,13 +112,12 @@ function App() {
           
         </div>
         <div className='scrolbar'>
-          <input id="brushSize" type="range" min="1" max="60" value="10" />
+          <input id="brushSize" type="range" min="1" max="60" />
         </div>
         
       </div>
     </div>
   )
 }
-
 
 export default App
